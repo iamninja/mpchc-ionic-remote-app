eb3aa72654b82ee7a89f17c61901fc38bed8eacf
Vagios Vlachos <bagios234@gmail.com>
2016-01-02 19:07:52 +0200

    Create POST requests

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,28 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $http) {
+  // Settings
+  ipAddress = '192.168.2.2';
+  port = '13579';
+  urlCommand = 'http://' + ipAddress + ':' + port +'/command.html';
+
+  // Execute POST commands
+  $scope.sendCommand = function(command, extra, extraValue) {
+    extra       = typeof extra !== 'undefined' ? extra : 'null';
+    extraValue  = typeof extraValue !== 'undefined' ? extraValue : '0';
+
+    data = {
+      'wm_command': command,
+    };
+
+    $http({
+      method: 'POST',
+      url: urlCommand,
+      data: 'wm_command=' + command + '&' + extra + '=' + extraValue,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    });
+  };
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
